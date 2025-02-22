d89e29829349b23f768eab03a8bde2737c299fd8
ETAvadh <developer.empiere@gmail.com>
2025-02-22 16:59:59 +0530

    - AndroidManifest: Defined meta data and intent filter tags for the deep linking. - Info.plist: Defined URL and enabled Flutter deep linking. - MainActivity: Defined logics to handle deep linking for android platform. - AppDelegate: Defined logics to handle deep linking for iOS platform. - main: Integrated everything in the UI with necessary formatting. - build.gradle: Refactored app name and app package.

--- android/app/src/main/kotlin/com/example/deeplink_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/deeplink_demo/MainActivity.kt
@@ -1,5 +1,32 @@
-package com.example.deeplink_demo
+package com.apexavadh.deeplink_demo
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.EventChannel.EventSink
+import io.flutter.plugin.common.MethodChannel
+import android.net.Uri
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "launchUrl"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "launchUrl")
+            .setMethodCallHandler { call, result ->
+                if (call.method == "openUrl") {
+                    val url = call.arguments as String
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                    startActivity(intent)
+                    result.success(null)
+                } else {
+                    result.notImplemented()
+                }
+            }
+    }
+}
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,35 @@
 import Flutter
 import UIKit
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let channel = FlutterMethodChannel(name: "launchUrl", binaryMessenger: controller.binaryMessenger)
+        channel.setMethodCallHandler { (call, result) in
+            if call.method == "openUrl" {
+                if let url = call.argument as? String {
+                    if let url = URL(String: url) {
+                        if UIApplication.shared.canOpenURL(url) {
+                            UIApplication.shared.open(url, option: [:], completionHandler: nil)
+                            result(nil)
+                        } else {
+                            result(FlutterError(code: "UNAVAILABLE", message: "URL cannot be opened", details: nil))
+                        }
+                    } else {
+                        result(FlutterError(code: "INVALID_URL", message: "Invalid URL", details: nil))
+                    }
+                } else {
+                    result(FlutterError(code: "INVALID_ARGUMENT", message: "Missing URL argument", details: nil))
+                }
+            } else {
+                result(FlutterMethodNotImplemented)
+            }
+        }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
